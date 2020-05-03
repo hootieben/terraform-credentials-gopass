0702ef2130ea542ac0c8295a2b9bb39edef430bc
Ben Sykes <hootieben@gmail.com>
2020-05-03 15:36:43 -0500

    Initial refactor

--- main.go
+++ main.go
@@ -6,29 +6,25 @@ import (
 	"log"
 	"os"
 	"os/exec"
+
+	gperr "github.com/gopasspw/gopass/pkg/action"
 )
 
 type resultJSON struct {
 	Token string `json:"token"`
 }
 
-func valArgs() {
-	args := os.Args[1:]
-	if len(args) < 2 {
-		fmt.Println()
-	}
-}
-
 func getCred(c string) (string, error) {
-	cmd := exec.Command("gopass", c)
+	cmd := exec.Command("gopass", "show", "-f", "-o", c)
 	out, err := cmd.CombinedOutput()
 	return string(out), err
 }
 
-func formatOutput(c string) (resultJSON []byte, err error) {
+func formatOutput(c string) (jsonresult []byte, err error) {
 	result := resultJSON{c}
-	resultJSON, err = json.Marshal(result)
-	return resultJSON, err
+	jsonresult, err = json.Marshal(result)
+
+	return jsonresult, err
 }
 
 func main() {
@@ -37,16 +33,18 @@ func main() {
 		fmt.Fprintln(os.Stderr, "Usage: terraform-credentials-gopass get <hostname>")
 		os.Exit(1)
 	}
+
 	switch args[0] {
 	case "get":
-		//cmd := exec.Command("gopass", args[1])
-		//out, err := cmd.CombinedOutput()
 		out, err := getCred(args[1])
 		if err != nil {
+			if exitError, ok := err.(*exec.ExitError); ok {
+				if exitError.ExitCode() == gperr.ExitDecrypt {
+					log.Fatalf("Unable to find or decrypt secrect %s\n", args[1])
+				}
+			}
 			log.Fatalf("Failed with %s\n", err)
 		}
-		//result := resultJSON{string(out)}
-		//resultJSON, err := json.Marshal(result)
 		result, err := formatOutput(out)
 		if err != nil {
 			log.Fatal(err)
@@ -55,7 +53,7 @@ func main() {
 		os.Stdout.WriteString("\n")
 		os.Exit(0)
 	default:
-		fmt.Fprintf(os.Stderr, "The 'env' credentials helper is not able to %s credentials.\n", args[0])
+		fmt.Fprintf(os.Stderr, "The gopass credentials helper is not able to %s credentials.\n", args[0])
 		os.Exit(1)
 	}
 }
